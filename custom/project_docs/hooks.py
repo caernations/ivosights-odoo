cfb012bb681a1f319dac609894612b33a2358235
Muhammad Davis Adhipramana <110291644+Loxenary@users.noreply.github.com>
2025-05-21 17:23:22 +0700

    Update hooks.py

--- custom/project_docs/hooks.py
+++ custom/project_docs/hooks.py
@@ -1,8 +1,5 @@
+# Delete the hooks in database
 def uninstall_hook(cr, registry):
-    """
-    This hook will clean up any references to our old models
-    when the module is uninstalled
-    """
     cr.execute("DELETE FROM ir_model WHERE model IN ('documentation.project', 'documentation.document')")
     cr.execute("DELETE FROM ir_model_fields WHERE model IN ('documentation.project', 'documentation.document')")
     cr.execute("DELETE FROM ir_model_relation WHERE model IN ('documentation.project', 'documentation.document')")
